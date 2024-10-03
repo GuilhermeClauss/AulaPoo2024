9727e2b3ca223b42117295ae9beac8faeda48695
GuilhermeClauss <guilhermeclauss@gmail.com>
2024-10-03 00:46:30 +0000

    aula

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+import { CadastroComponent } from './autocadastro/autocadastro.component';
+import { TelaPixComponent } from './tela-pix/tela-pix.component';
+
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'tela-inicial', component: TelaInicialComponent},
-    {path: 'cadastro', component: CadastroComponent}
+  { path: '', component: LoginComponent },
+  { path: 'tela-inicial', component: TelaInicialComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'tela-pix', component: TelaPixComponent }
 ];
---
+++ front/src/app/tela-inicial/tela-inicial.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tela-inicial',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-inicial.component.html',
+  styleUrl: './tela-inicial.component.css'
+})
+
+export class TelaInicialComponent {
+  constructor(private router: Router) {
+
+  }
+  public fazerumpix() {
+    this.router.navigate(['/tela-pix']);
+    }
+}
